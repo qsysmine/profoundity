fafb1b67fb9d7becd668b0249fac341767833fba
qsysmine <echomanari@gmail.com>
2014-11-28 10:26:11 -0800

    Update index.js

--- save/index.js
+++ save/index.js
@@ -3,7 +3,7 @@
   var unCode = new Date().getTime() * 2
   $('input').keypress(function(e) {
     if(e.keyCode == 13) {
-      data.child("saves").child(unCode).push({top:$('#top').val(),middle:$('#middle').val(),bottom:$('#bottom').val()});
+      data.child("saves").child(unCode).set({top:$('#top').val(),middle:$('#middle').val(),bottom:$('#bottom').val()});
       $('#form').fadeOut(500, function() {
         $('#link').attr("href","/l/#" + unCode);
         $('#link').slideDown(500);
