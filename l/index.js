7c9794b372c381ece4ba0cab312699802fce3a71
qsysmine <echomanari@gmail.com>
2014-11-28 10:37:44 -0800

    Update index.js

--- l/index.js
+++ l/index.js
@@ -1,1 +1,16 @@
-(function() {})();
+(function() {
+  var data = new Firebase("https://profoundity-save.firebaseio.com/");
+  if(location.hash != "") {
+    var unCode = parseInt(location.hash.split("#")[1]);
+    var obj = data.child(saves).child(unCode);
+    obj.on("value", function(snap) {
+      var top, middle, bottom;
+      top = snap.val().top;
+      middle = snap.val().middle;
+      bottom = snap.val().bottom;
+      $('#top').text(top);
+      $('#middle').text(middle);
+      $('#bottom').text(bottom);
+    });
+  }
+})();
