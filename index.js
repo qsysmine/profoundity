32f3d70a939107c14ce79dfaefec7477310b07f4
qsysmine <echomanari@gmail.com>
2014-11-28 10:56:21 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 (function() {
-  var hash = location.hash;
-  var comm = hash.split("#")[1];
-  var parts = comm.split("/");
-  var top = parts[0];
-  var middle = parts[1];
-  var bottom = parts[2];
-  $('#top').text(top.toUpperCase());
-  $('#middle').text(middle);
-  $('#bottom').text(bottom.toUpperCase() + ".");
+  if(location.hash == "") {
+    $('#top,#middle,#bottom').hide();
+    $('#explanation').fadeIn();
+  } else {
+    var hash = location.hash;
+    var comm = hash.split("#")[1];
+    var parts = comm.split("/");
+    var top = parts[0];
+    var middle = parts[1];
+    var bottom = parts[2];
+    $('#top').text(top.toUpperCase());
+    $('#middle').text(middle);
+    $('#bottom').text(bottom.toUpperCase() + ".");
+  }
 })();
